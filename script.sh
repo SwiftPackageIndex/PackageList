8245ab742296b01a27c17659eb6ef1e5a474fc95
leogdion <leogdion@brightdigit.com>
2020-03-12 10:13:53 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 temp_file=$(mktemp)
 curl -s https://raw.githubusercontent.com/brightdigit/SwiftPMLibrary/feature/package-validation/validate.swift > $temp_file
 swift $temp_file $PWD $*
