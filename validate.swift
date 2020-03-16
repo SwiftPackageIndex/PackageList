f627a96affa5cd7b0672cbfae79bbf893af406f6
leogdion <leogdion@brightdigit.com>
2020-03-16 19:52:44 -0400

    setting simul process to 5

--- validate.swift
+++ validate.swift
@@ -5,7 +5,7 @@ import Foundation
 // MARK: Configuration Values and Constants
 
 // number of validations to run simultaneously
-let semaphoreCount = 1
+let semaphoreCount = 5
 
 let timeoutIntervalForRequest = 3000.0
 let timeoutIntervalForResource = 6000.0
