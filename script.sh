79afd05c9f42494ecc682924156d0e36b3493c35
leogdion <leogdion@brightdigit.com>
2020-03-25 14:16:02 -0400

    fixing script for master

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 temp_file=$(mktemp)
-curl -s https://raw.githubusercontent.com/brightdigit/SwiftPMLibrary/feature/package-validation/validate.swift > $temp_file
+curl -s https://raw.githubusercontent.com/brightdigit/SwiftPMLibrary/master/validate.swift > $temp_file
 swift $temp_file $PWD $*
 rm $temp_file
