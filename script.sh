9d7ccaebc285d1be3f733c608334aa9071d133b1
John Mueller <jmuellerokc@gmail.com>
2020-06-19 07:55:20 -0500

    Updated URLs

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # To run in local Swift package directory execute:
-# `curl -s https://raw.githubusercontent.com/daveverwer/SwiftPMLibrary/master/script.sh | bash -s -- mine`
+# `curl -s https://raw.githubusercontent.com/SwiftPackageIndex/PackageList/main/script.sh | bash -s -- mine`
 
 temp_file=$(mktemp)
-curl -s https://raw.githubusercontent.com/daveverwer/SwiftPMLibrary/master/validate.swift > $temp_file
+curl -s https://raw.githubusercontent.com/SwiftPackageIndex/PackageList/main/validate.swift > $temp_file
 swift $temp_file $PWD $*
 rm $temp_file
