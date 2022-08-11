7e46bfb1f4f3b7adad2f9cbb5209275cfd524d40
Sven A. Schmidt <sas@finestructure.co>
2022-08-11 16:34:39 +0200

    Only show stats for the most recent weeks

--- .stats.swift
+++ .stats.swift
@@ -10,9 +10,11 @@ fmt.dateFormat = "yyyy-MM-dd"
 fmt.timeZone = .init(abbreviation: "UTC")
 
 let start = fmt.date(from: "2019-05-15")!
+let reportFrom = calendar.date(byAdding: .day, value: -30, to: .now)!
 var date = start
 while date <= Date.now {
     defer { date = calendar.date(byAdding: .init(day: 7), to: date)! }
+    if date < reportFrom { continue }
 
     let rev = try shellOut(to: #"git rev-list -n 1 --before="\#(date)" main"#)
 
