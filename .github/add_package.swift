9e7728e56b4c733ffd9638f2c51b6f17402ba145
Sven A. Schmidt <sas@finestructure.co>
2024-06-28 11:14:04 +0200

    Update add_package.swift

--- .github/add_package.swift
+++ .github/add_package.swift
@@ -7,7 +7,7 @@ func loadPackages(at path: String) throws -> [URL] {
 
 func savePackages(_ packages: [URL], to path: String) throws {
     let encoder = JSONEncoder()
-    encoder.outputFormatting = [.withoutEscapingSlashes, .prettyPrinted]
+    encoder.outputFormatting = [.prettyPrinted, .withoutEscapingSlashes]
     let data = try encoder.encode(packages)
     try data.write(to: URL(fileURLWithPath: path))   
 }
@@ -22,29 +22,28 @@ func loadDenyList() throws -> [URL] {
 
 func main() throws {
     guard let body = ProcessInfo.processInfo.environment["GH_BODY"] else {
-        print("No body")
+        print("Body (GH_BODY) not set")
         exit(1)
     }
     
     var packages = try loadPackages(at: "packages.json")
     
     for line in body.split(whereSeparator: \.isWhitespace) {
-        let line = String(line)
-        guard let url = URL(string: line) else {
+        guard let url = URL(string: String(line)) else {
             print("Invalid url:", line)
             exit(1)
         }
         if !packages.contains(url) {
             packages.append(url)
-            print("+ \(url).")
+            print("+ \(url)")
         }
     }
     
     let denyList = Set(try loadDenyList().map { $0.absoluteString.lowercased() })
-    let result = packages
+    let filtered = packages
         .filter { !denyList.contains($0.absoluteString.lowercased()) }
         .sorted { $0.absoluteString.lowercased() < $1.absoluteString.lowercased() }
-    try savePackages(result, to: "packages.json")
+    try savePackages(filtered, to: "packages.json")
 }
 
 try main()
